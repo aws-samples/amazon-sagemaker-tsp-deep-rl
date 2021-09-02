e229fe8be070b610f955ae0520325d338213e7e1
Josiah Davis <josiah.j.davis@gmail.com>
2021-09-02 10:33:44 -0500

    Add setup file script to move files SageMaker needs to common directory

---
+++ set_up_sagemaker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r learning-tsp/{nets,problems,reinforce_baselines.py,utils} src
+# mkdir learning-tsp/src
+# mv learning-tsp/{nets,problems,reinforce_baselines.py,utils} learning-tsp/src
+# mv src/*.py learning-tsp/src
