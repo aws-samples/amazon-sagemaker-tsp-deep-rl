07ce87d2e6e56c41b3967489c3aee71016880342
Josiah Davis <josiah.j.davis@gmail.com>
2021-09-02 12:26:06 -0500

    Cleanup sagemaker cleanup script

--- set_up_sagemaker.sh
+++ set_up_sagemaker.sh
@@ -5,7 +5,5 @@ model_path=learning-tsp/pretrained/tsp_20-50/rl-ar-var-20pnn-gnn-max_20200313T00
 mv $model_path/{epoch-99.pt,model.pt}
 tar -cvzf $model_path/model.tar.gz $model_path/
 
+echo "Moving training and inference files to single source directory..."
 cp -r learning-tsp/{nets,problems,reinforce_baselines.py,utils} src
-# mkdir learning-tsp/src
-# mv learning-tsp/{nets,problems,reinforce_baselines.py,utils} learning-tsp/src
-# mv src/*.py learning-tsp/src
