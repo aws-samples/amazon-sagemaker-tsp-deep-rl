f443498a379dfa4cf6698c21ff1a616de5266edc
Josiah Davis <josiah.j.davis@gmail.com>
2021-09-02 12:36:36 -0500

    Fix compression of model files

--- set_up_sagemaker.sh
+++ set_up_sagemaker.sh
@@ -3,7 +3,9 @@
 echo "Compressing pretrained model..."
 model_path=learning-tsp/pretrained/tsp_20-50/rl-ar-var-20pnn-gnn-max_20200313T002243
 mv $model_path/{epoch-99.pt,model.pt}
-tar -cvzf $model_path/model.tar.gz $model_path/
+cd $model_path
+tar -cvzf model.tar.gz *
 
 echo "Moving training and inference files to single source directory..."
+cd -
 cp -r learning-tsp/{nets,problems,reinforce_baselines.py,utils} src
