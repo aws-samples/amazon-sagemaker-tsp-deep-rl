b23d122d043206b244544ed3b747187200f2a28c
Josiah Davis <josiah.j.davis@gmail.com>
2021-09-08 16:52:36 -0500

    Fix remove superfluos mv command

--- scripts/set_up_sagemaker.sh
+++ scripts/set_up_sagemaker.sh
@@ -6,7 +6,6 @@ pip install -r requirements.txt
 
 echo "Compressing pretrained model..."
 model_path=learning-tsp/pretrained/tsp_20-50/rl-ar-var-20pnn-gnn-max_20200313T002243
-mv $model_path/{epoch-99.pt,args.pt}
 cd $model_path
 mv epoch-99.pt model.pt
 tar -cvzf model.tar.gz *
