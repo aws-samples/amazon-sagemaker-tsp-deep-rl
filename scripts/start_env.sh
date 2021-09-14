89b143c1a22730fe2e9882d4df2f31337244dee4
Josiah Davis <josiah.j.davis@gmail.com>
2021-09-14 11:58:54 -0500

    Update start_env.sh to include restart so env
    Rationale: Required for notebook to show the virtual environment.

--- scripts/start_env.sh
+++ scripts/start_env.sh
@@ -6,5 +6,21 @@ line=$(head -n 1 environment.yml)
 ENV_NAME="${line/name:\ /}"
 PWD=$(pwd)
 
+# fix an issue for displaying plotly
+# jupyter labextension install jupyterlab-plotly
+
 source "$WORKING_DIR/miniconda/bin/activate"
 conda activate $ENV_NAME
+
+# Cleanup
+conda deactivate
+source "${WORKING_DIR}/miniconda/bin/deactivate"
+
+# Add the following env dir to envs_dirs
+conda config --add envs_dirs "$PWD/$WORKING_DIR/miniconda/envs"
+
+# Activate the kernel by list the envs
+conda env list
+
+# Optional
+sudo initctl restart jupyter-server --no-wait
